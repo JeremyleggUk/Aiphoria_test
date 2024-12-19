90488168ce1846b282ac2dd08612c065c02d2ee6
Jeremy Legg <jeremy.legg.uk@outlook.com>
2024-12-19 23:14:23 +0000

    Submission for Aiphoria tech test

---
+++ LotteryNumberGenerator/LotteryNumberGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace LotteryNumberGenerator
+{
+    public static class LotteryNumberGenerator
+    {
+        public static List<int> GenerateRandomNumbersForLength(int min, int max, int length)
+        {
+            if (min >= max || length < 1)
+            {
+                throw new ArgumentException("Invalid min/max or length");
+            }
+
+            Random random = new Random();
+            var randomNumbers = new List<int>();
+
+            while (randomNumbers.Count < length)
+            {
+                var randomNumber = random.Next(min, max);
+                if (!randomNumbers.Contains(randomNumber))
+                {
+                    randomNumbers.Add(randomNumber);
+                }
+            }
+
+            randomNumbers.Sort();
+
+            return randomNumbers;
+        }
+    }
+}
